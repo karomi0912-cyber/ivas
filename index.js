258b1f6b36d9deb6cbf83460e0722c00f8874324
karomi0912-cyber <karomi0912@gmail.com>
2026-05-26 18:50:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express = require("express");
-const ivasmsRouter = require("./router.js"); 
-
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000; // Railway inject PORT otomatis
+const ivasms = require('./router');
 
-app.use(express.json()); 
-app.use("/api/ivasms", ivasmsRouter);
+app.use(express.json());
+app.use('/api/ivasms', ivasms);
 
-app.get("/", (req, res) => res.send("✅ IVASMS API jalan"));
+app.get('/', (req, res) => res.send('IVASMS API Online'));
 
-app.listen(PORT, () => console.log(`Server jalan di port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server jalan di ${PORT}`));
