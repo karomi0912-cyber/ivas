0c418fecdf1e0c8f83528ca3abf9337bc362d33f
karomi0912-cyber <karomi0912@gmail.com>
2026-05-26 19:24:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const ivasms = require('./router');
 
+const COOKIES = process.env.IVAS_COOKIE || ''; // <-- TAMBAHIN INI DI SINI
+
 app.use(express.json());
 app.use('/api/ivasms', ivasms);
 
-app.get('/', (req, res) => res.send('IVASMS API Online'));
+app.get('/', (req, res) => res.send('IVASMS API'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server jalan di ${PORT}`));
